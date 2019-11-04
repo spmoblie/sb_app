4a1ba512102ec354c2df59bfc55e2a55afa7dbbd
Evan1985 <Evan1985@126.com>
2019-11-04 18:13:49 +0800

    phone

--- Sampo/app/src/main/java/com/songbao/sampo/activity/login/ResetPasswordActivity.java
+++ Sampo/app/src/main/java/com/songbao/sampo/activity/login/ResetPasswordActivity.java
@@ -95,11 +95,59 @@ public class ResetPasswordActivity extends BaseActivity implements OnClickListen
 
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				//添加数字
 				if (count == 1) {
-					int length = s.toString().length();
-					if (length == 3 || length == 8) {
-						et_phone.setText(s + " ");
-						et_phone.setSelection(et_phone.getText().length());
+					String str = s.toString();
+					int length = str.length();
+					if (length > 3 && length < 9) {
+						String str_4 = String.valueOf(str.charAt(3));
+						if (!str_4.equals(" ")) {
+							if (str.contains(" ")) {
+								str = str.replaceAll(" ", "");
+							}
+							length = str.length();
+							if (length > 7) {
+								length = 7;
+							}
+							String part1 = str.substring(0, 3);
+							String part2 = str.substring(3, length);
+							et_phone.setText(part1 + " " + part2);
+							if (start == 3) {
+								start += 2;
+							} else {
+								start += 1;
+							}
+							if (start < et_phone.getText().length()) {
+								et_phone.setSelection(start);
+							} else {
+								et_phone.setSelection(et_phone.getText().length());
+							}
+						}
+					}else
+					if (length >= 9){
+						String str_4 = String.valueOf(str.charAt(3));
+						String str_9 = String.valueOf(str.charAt(8));
+						if (!str_4.equals(" ") || !str_9.equals(" ")) {
+							str = str.replaceAll(" ", "");
+							length = str.length();
+							if (length > 11) {
+								length = 11;
+							}
+							String part1 = str.substring(0, 3);
+							String part2 = str.substring(3, 7);
+							String part3 = str.substring(7, length);
+							et_phone.setText(part1 + " " + part2 + " " + part3);
+							if (start == 3 || start == 8) {
+								start += 2;
+							} else {
+								start += 1;
+							}
+							if (start < et_phone.getText().length()) {
+								et_phone.setSelection(start);
+							} else {
+								et_phone.setSelection(et_phone.getText().length());
+							}
+						}
 					}
 				}
 			}
